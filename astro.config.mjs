6d9aeff4c63dee7e6c15bb176a1c2945ef4f40a0
nora byrne <fionneomuiri@gmail.com>
2026-03-11 17:57:15 -0400

    Added deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://fionneom.github.io',
-  base: '/', // Use '/' for your specific username-repo setup
+  base: '/',
 });
